1c74e9c3ce3cb28c1fb60763f08267371767dd9f
sonharang <bhson3@naver.com>
2023-10-25 23:36:28 +0900

    10.25 23:35

--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -1,8 +1,36 @@
 const week = ['일','월','화','수','목','금','토'];
-const month = [1,2,3,4,5,6,7,8,9,10,11,12];
-const day = new Array();
-	for(i=1;i<=31;i++){
-		day.push(i);
-	}
-	console.log(day);
+const month = [31,28,31,30,31,30,31,31,30,31,30,31];
+const calendar = {
+	html : '',
 	
+	makeHead(){
+		let strHead ='';
+		strHead += `<table border = '1'><thead><tr>`;
+		for(let weekday in week){
+			strHead += '<th>' + week[weekday] + '</th>';
+		}
+		strHead += '</tr></thead>';
+		this.html += strHead;
+	},
+	makeBody(monthday){
+		let obj = this;
+		let strBody = '';
+		strBody += '<tbody><tr>'
+		for(let i =1; i<=month[monthday]; i++){
+			strBody += `<td>${i}</td>`
+			if(i%7==0){
+				strBody +='<tr></tr>'
+			}
+		}
+		strBody += '</tr></tbody</table><br>'
+		obj.html += strBody;
+	},
+	makeCalendar(dom){
+		dom.innerHTML = this.html;
+	}
+}
+for(let i = 0; i<12; i++){
+	calendar.makeHead();
+	calendar.makeBody(i);
+} 
+	calendar.makeCalendar(document.getElementById('show'));
